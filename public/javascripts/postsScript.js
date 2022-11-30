93e568580bee5c505f6a8fa614a407e229748b55
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2022-11-30 08:30:06 -0500

    Set image files not found on ogs to placeholder

--- public/javascripts/postsScript.js
+++ public/javascripts/postsScript.js
@@ -11,11 +11,13 @@ externalLinks.forEach((link) => {
 });
 
 externalLinks.forEach(async (link) => {
+  const linkUrl = link.href.split("://")[1];
   const linkDiv = link.parentElement.children[1];
 
-  const result = await fetch(
-    "http://localhost:8080/open-graph/www.patreon.com"
-  );
+  console.log(linkUrl);
+  // return;
+
+  const result = await fetch(`http://localhost:8080/open-graph/${linkUrl}`);
 
   const data = await result.json();
 
--- routes/open-graph.js
+++ routes/open-graph.js
@@ -8,27 +8,31 @@ router.get("/:id", async (req, res, next) => {
   const site = req.params.id;
   const options = { url: site };
   const data = await ogs(options);
+  console.log(data.result);
 
-  if (data.error == true) {
+  if (data.error === true) {
     return res.send(new Error("There was an error fetching site data"));
   }
 
+  const result = data.result;
   let siteInfo;
   if (data.result.success) {
     siteInfo = {
-      name: data.result.ogSiteName ?? data.result.ogTitle ?? "Not Found",
-      description: data.result.ogDescription ?? "Not Found",
-      url: data.result.ogUrl ?? "Not Found",
-      img: data.result.ogImage ? data.result.ogImage.url : "Not Found"
+      name: result.ogSiteName ?? result.ogTitle ?? "Not Found",
+      description: result.ogDescription ?? "Not Found",
+      url: result.ogUrl ?? result.requestUrl ?? "Not Found",
+      img: result.ogImage ? result.ogImage.url : "/images/post-placeholder.png"
     };
   } else {
     siteInfo = {
       name: "Not Found",
       description: "Not Found",
-      url: "Not Found",
-      img: "Not Found"
+      url: site,
+      img: "/images/post-placeholder.png"
     };
   }
+
+  console.log(siteInfo);
 
   res.send(siteInfo);
 });
