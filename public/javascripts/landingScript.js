4c9e25e17f04de7f860057f4e11c454e1ab2f5b7
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2022-12-19 13:10:58 -0500

    Style and set up down to articles

--- public/javascripts/landingScript.js
+++ public/javascripts/landingScript.js
@@ -1,9 +1,23 @@
 "use strict";
 
+// Nav Buttons
 const register_btn = document.querySelector("#register");
 const browse_btn = document.querySelector("#browse");
 const about_btn = document.querySelector("#about");
 
+// Examples Section
+const examples_btns = document.querySelectorAll(".example-nav-btn");
+const example_a = document.querySelector("#ex-a");
+const example_b = document.querySelector("#ex-b");
+const example_c = document.querySelector("#ex-c");
+const example_d = document.querySelector("#ex-d");
+// const example_video_btn = document.querySelector("#video");
+// const example_podcast_btn = document.querySelector("#podcast");
+// const example_art_btn = document.querySelector("#art");
+// const example_music_btn = document.querySelector("#music");
+// const example_other_btn = document.querySelector("#other");
+
+// --------------------- Nav Buttons -----------------
 register_btn.addEventListener("click", () => {
   location.href = "/register";
 });
@@ -14,4 +28,70 @@ browse_btn.addEventListener("click", () => {
 
 about_btn.addEventListener("click", () => {
   location.href = "/about";
+});
+
+// ---------------- Example Creators Section -------------
+examples_btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    examples_btns.forEach((button) =>
+      button.classList.remove("example-nav-selected")
+    );
+
+    e.target.classList.add("example-nav-selected");
+
+    if (e.target.id === "video") {
+      example_a.childNodes[0].src =
+        "https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2022/08/Mega-64.png";
+      example_b.childNodes[0].src =
+        "https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2022/08/01_video_02.png";
+      example_c.childNodes[0].src =
+        "https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2022/08/01_video_03.png";
+      example_d.childNodes[0].src =
+        "https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2022/08/Channel-5.png";
+    }
+
+    if (e.target.id === "podcast") {
+      example_a.childNodes[0].src =
+        "https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2022/08/02_pod_01.png";
+      example_b.childNodes[0].src =
+        "https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2022/08/02_pod_02.png";
+      example_c.childNodes[0].src =
+        "https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2022/08/02_pod_05.png";
+      example_d.childNodes[0].src =
+        "https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2022/08/02_pod_04.png";
+    }
+
+    if (e.target.id === "art") {
+      example_a.childNodes[0].src =
+        "https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2022/08/03_vis_01.png";
+      example_b.childNodes[0].src =
+        "https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2022/08/03_vis_02.png";
+      example_c.childNodes[0].src =
+        "https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2022/08/03_vis_03.png";
+      example_d.childNodes[0].src =
+        "https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2022/08/03_vis_05.jpg";
+    }
+
+    if (e.target.id === "music") {
+      example_a.childNodes[0].src =
+        "https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2022/08/04_music_01.png";
+      example_b.childNodes[0].src =
+        "https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2022/08/04_music_02.png";
+      example_c.childNodes[0].src =
+        "https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2022/08/04_music_03.png";
+      example_d.childNodes[0].src =
+        "https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2022/08/04_music_04.png";
+    }
+
+    if (e.target.id === "other") {
+      example_a.childNodes[0].src =
+        "https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2022/08/05_other_01.png";
+      example_b.childNodes[0].src =
+        "https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2022/08/05_other_02.png";
+      example_c.childNodes[0].src =
+        "https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2022/08/05_other_03.png";
+      example_d.childNodes[0].src =
+        "https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2022/08/05_other_04.png";
+    }
+  });
 });
