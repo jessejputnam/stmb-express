eb920da3498139470ee820b295dafdddc8dda41b
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2022-12-23 11:34:31 -0500

    Style mobilenoauth  modal

--- public/javascripts/noAuthScript.js
+++ public/javascripts/noAuthScript.js
@@ -8,24 +8,20 @@ const mid_bar = document.querySelector(".barMid");
 const low_bar = document.querySelector(".barLow");
 
 hamburger.addEventListener("click", () => {
-  if (hamburger.classList.contains("modal-open")) {
+  if (modal.classList.contains("modal-open")) {
     window.scrollTo(0, 0);
     header.classList.remove("sticky");
-    modal.classList.add("hidden");
+    modal.classList.remove("modal-open");
 
     top_bar.classList.remove("barTop-open");
     mid_bar.classList.remove("barMid-open");
     low_bar.classList.remove("barLow-open");
-
-    hamburger.classList.remove("modal-open");
   } else {
     header.classList.add("sticky");
-    modal.classList.remove("hidden");
+    modal.classList.add("modal-open");
 
     top_bar.classList.add("barTop-open");
     mid_bar.classList.add("barMid-open");
     low_bar.classList.add("barLow-open");
-
-    hamburger.classList.add("modal-open");
   }
 });
