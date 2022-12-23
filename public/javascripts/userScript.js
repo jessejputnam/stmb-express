9c3ae2afec4c5ec0daf841f49b904d7c2d3f5224
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2022-12-23 14:05:29 -0500

    Get auth nav bar mobile friendly

--- public/javascripts/userScript.js
+++ public/javascripts/userScript.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// Nav Button Selection
+
 const nav_home = document.querySelector("#home");
 const nav_browse = document.querySelector("#browse");
 const nav_search = document.querySelector("#search");
@@ -16,3 +18,30 @@ if (location.pathname === "/search")
 
 if (location.pathname === "/home/settings")
   nav_settings.classList.add("selected-sidebar-nav");
+
+// HAMBURGER MENU
+const header = document.querySelector("header");
+const modal = document.querySelector(".mobile-modal");
+const hamburger = document.querySelector(".hamburger");
+const top_bar = document.querySelector(".barTop");
+const mid_bar = document.querySelector(".barMid");
+const low_bar = document.querySelector(".barLow");
+
+hamburger.addEventListener("click", () => {
+  if (modal.classList.contains("modal-open")) {
+    window.scrollTo(0, 0);
+    header.classList.remove("sticky");
+    modal.classList.remove("modal-open");
+
+    top_bar.classList.remove("barTop-open");
+    mid_bar.classList.remove("barMid-open");
+    low_bar.classList.remove("barLow-open");
+  } else {
+    header.classList.add("sticky");
+    modal.classList.add("modal-open");
+
+    top_bar.classList.add("barTop-open");
+    mid_bar.classList.add("barMid-open");
+    low_bar.classList.add("barLow-open");
+  }
+});
