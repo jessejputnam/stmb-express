d561dceccb62f9ba503487ab877f07a2aff30c89
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2022-12-30 16:10:28 -0500

    More styling of edit button for posts

--- public/javascripts/postsViewScript.js
+++ public/javascripts/postsViewScript.js
@@ -2,8 +2,13 @@
 
 const list_btn = document.querySelector("#list-view");
 const post_btn = document.querySelector("#post-view");
+
 const posts_abbr = document.querySelector(".abbr-posts-container");
 const posts_full = document.querySelector(".posts-container");
+
+const read_post_btns = document.querySelectorAll(".read-post-btn");
+
+// Toggle View Buttons
 
 list_btn.addEventListener("click", () => {
   if (list_btn.classList.contains("toggle-view-selected")) return;
@@ -21,4 +26,18 @@ post_btn.addEventListener("click", () => {
   list_btn.classList.remove("toggle-view-selected");
   posts_full.classList.remove("hidden");
   posts_abbr.classList.add("hidden");
+});
+
+// Read Post Buttons
+
+read_post_btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const clicked = e.target;
+    const post_container = clicked.parentElement.parentElement;
+    const idx = post_container.children.length - 1;
+    const post_text_container = post_container.children[idx];
+    post_text_container.classList.remove("hidden");
+    clicked.parentElement.classList.add("hidden");
+    console.log(clicked.parentElement);
+  });
 });
